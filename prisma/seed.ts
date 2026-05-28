2d85b225332ff6b05f9e40604788396d2c744c67
Allan <allanrs19@gmail.com>
2026-05-28 17:35:22 -0400

    fixed wrong import for seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import prisma from '@/lib/db';
 import { Role } from '@/app/generated/prisma/enums';
+import prisma from '@/lib/db/prisma';
 import bcrypt from 'bcryptjs';
 
 async function main() {
